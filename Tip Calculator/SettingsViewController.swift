48c12cea28bf0719b280f291541e38802ea3eead
bigmak712 <bigmak712@gmail.com>
2016-12-17 17:36:31 -0800

    Fixed rounded tip/total, split check total and added background color. I think I'm done... might need to look for bugs

--- Tip Calculator/SettingsViewController.swift
+++ Tip Calculator/SettingsViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    @IBOutlet weak var setDefaultTipTitle: UILabel!
+    @IBOutlet weak var setFirstTipTitle: UILabel!
+    @IBOutlet weak var setSecondTipTitle: UILabel!
+    @IBOutlet weak var setThirdTipTitle: UILabel!
+    
     @IBOutlet weak var settingsTipSegment: UISegmentedControl!
     @IBOutlet weak var roundedButton: UIButton!
     @IBOutlet weak var splitButton: UIButton!
@@ -30,12 +35,34 @@ class SettingsViewController: UIViewController {
     let customTipKey2 = "custom tip key 2"
     let customTipKey3 = "custom tip key 3"
     
+    let buttonBorderWidth = CGFloat(1)
+    let buttonCornerRadius = CGFloat(5)
+    
+    let customGreen = UIColor(red:0.1098, green: 0.4078, blue: 0.0118, alpha: 1.0)
+    
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = customGreen
+        setDefaultTipTitle.textColor = UIColor.white
+        setFirstTipTitle.textColor = UIColor.white
+        setSecondTipTitle.textColor = UIColor.white
+        setThirdTipTitle.textColor = UIColor.white
+        settingsTipSegment.tintColor = UIColor.white
+        roundedButton.setTitleColor(UIColor.white, for: .normal)
+        roundedButton.layer.borderColor = UIColor.white.cgColor
+        roundedButton.layer.borderWidth = buttonBorderWidth
+        roundedButton.layer.cornerRadius = buttonCornerRadius
+        splitButton.setTitleColor(UIColor.white, for: .normal)
+        splitButton.layer.borderColor = UIColor.white.cgColor
+        splitButton.layer.borderWidth = buttonBorderWidth
+        splitButton.layer.cornerRadius = buttonCornerRadius
+        customTip1.textColor = UIColor.white
+        customTip2.textColor = UIColor.white
+        customTip3.textColor = UIColor.white
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,21 +84,28 @@ class SettingsViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        //load the segmented control values
+        // Load the default tip percentages
         var custom1 = defaults.double(forKey: customTipKey1)
         var custom2 = defaults.double(forKey: customTipKey2)
         var custom3 = defaults.double(forKey: customTipKey3)
         
+        // Update the tipPercentages array values
+        tipPercentages[0] = custom1
+        tipPercentages[1] = custom2
+        tipPercentages[2] = custom3
+        
+        // Multiply the default tip percentages by 100
         custom1 = custom1 * 100
         custom2 = custom2 * 100
         custom3 = custom3 * 100
         
+        // Set the segment control titles to the 3 default tip percentages
         settingsTipSegment.setTitle(String(format: "%.0f", custom1) + "%", forSegmentAt: 0)
         settingsTipSegment.setTitle(String(format: "%.0f", custom2) + "%", forSegmentAt: 1)
         settingsTipSegment.setTitle(String(format: "%.0f", custom3) + "%", forSegmentAt: 2)
 
         
-        //load the default tip key
+        // Load the default tip key
         tipPercent = defaults.double(forKey: tipKey)
         
         if(tipPercent == tipPercentages[0]){
@@ -84,13 +118,16 @@ class SettingsViewController: UIViewController {
             settingsTipSegment.selectedSegmentIndex = 2
         }
         
-        //load the rounded key
+        // Load the rounded key
         let rounded = defaults.bool(forKey: roundedKey)
         
+        // Rounded enabled - set the button to hide and set the rounded boolean to true
         if(rounded){
             roundedButton.setTitle("Hide Rounded Tip/Total", for: .normal)
             roundedShown = true
         }
+            
+        // Rounded disabled - set the button to show and set the rounded boolean to false
         else{
             roundedButton.setTitle("Show Rounded Tip/Total", for: .normal)
             roundedShown = false
@@ -99,10 +136,13 @@ class SettingsViewController: UIViewController {
         //load the split key
         let split = defaults.bool(forKey: splitKey)
         
+        // Split Check enabled - set the button to disable and set the split boolean to true
         if(split){
             splitButton.setTitle("Disable Split Check Option", for: .normal)
             splitShown = true
         }
+            
+        // Split Check disabled - set the button to enable and set the split boolean to false
         else{
             splitButton.setTitle("Enable Split Check Option", for: .normal)
             splitShown = false
@@ -110,19 +150,88 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func changeCustomTip1(_ sender: Any) {
+        
+        // Set tip1 to the text for the Custom Tip 1 text field
         var tip1 = (Double(customTip1.text!) ?? tipPercentages[0])
         
-        if(tip1 > 0){
+        // tip1 value is between 0 and 100 exclusive
+        if(tip1 > 0 && tip1 < 100){
             
+            // Set the first segment to the new tip value
             settingsTipSegment.setTitle(String(format: "%.0f", tip1) + "%", forSegmentAt: 0)
             
+            // Divide the tip by 100 to get the percentage value
             tip1 = tip1 / 100
             
+            // Update the tip percentage in the tipPercentages array
             tipPercentages[0] = tip1
+            
+            // Update the tip key if the selected tip was changed
+            if(settingsTipSegment.selectedSegmentIndex == 0){
+                defaults.set(tip1, forKey: tipKey)
+            }
+            
+            // Save the new default tip percentage
             defaults.set(tip1, forKey: customTipKey1)
             defaults.synchronize()
+        }
+
+    }
+    
+    @IBAction func changeCustomTip2(_ sender: Any) {
+        
+        // Set tip2 to the text for the Custom Tip 2 text field
+        var tip2 = (Double(customTip2.text!) ?? tipPercentages[1])
+        
+        // tip2 value is between 0 and 100 exclusive
+        if(tip2 > 0 && tip2 < 100){
             
-            setDefaultTip(self)
+            // Set the second segment to the new tip value
+            settingsTipSegment.setTitle(String(format: "%.0f", tip2) + "%", forSegmentAt: 1)
+            
+            // Divide the tip by 100 to get the percentage value
+            tip2 = tip2 / 100
+            
+            // Update the tip percentage in the tipPercentages array
+            tipPercentages[1] = tip2
+            
+            // Update the tip key if the selected tip was changed
+            if(settingsTipSegment.selectedSegmentIndex == 1){
+                defaults.set(tip2, forKey: tipKey)
+            }
+            
+            // Save the new default tip percentage
+            defaults.set(tip2, forKey: customTipKey2)
+            defaults.synchronize()
+        }
+
+    }
+    
+    @IBAction func changeCustomTip3(_ sender: Any) {
+        
+        // Set tip3 to the text for the Custom Tip 3 text field
+        var tip3 = (Double(customTip3.text!) ?? tipPercentages[2])
+        
+        // tip3 value is between 0 and 100 exclusive
+        if(tip3 > 0 && tip3 < 100){
+            
+            // Set the third segment to the new tip value
+            settingsTipSegment.setTitle(String(format: "%.0f", tip3) + "%", forSegmentAt: 2)
+            
+            // Divide the tip by 100 to get the percentage value
+            tip3 = tip3 / 100
+            
+            // Update the tip percentage in the tipPercentages array
+            tipPercentages[2] = tip3
+            
+            // Update the tip key if the selected tip was changed
+            if(settingsTipSegment.selectedSegmentIndex == 2){
+                defaults.set(tip3, forKey: tipKey)
+            }
+            
+            // Save the new default tip percentage
+            defaults.set(tip3, forKey: customTipKey3)
+            defaults.synchronize()
         }
 
     }
--- Tip Calculator/TipViewController.swift
+++ Tip Calculator/TipViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var billTitle: UILabel!
     @IBOutlet weak var tipTitle: UILabel!
     @IBOutlet weak var totalTitle: UILabel!
+    @IBOutlet weak var barDivider: UIView!
     
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
@@ -36,15 +38,37 @@ class ViewController: UIViewController {
     let customTipKey2 = "custom tip key 2"
     let customTipKey3 = "custom tip key 3"
     
+    let customGreen = UIColor(red:0.1098, green: 0.4078, blue: 0.0118, alpha: 1.0)
+    
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        tipPercent = tipPercentages[tipControl.selectedSegmentIndex]
-        defaults.set(tipPercent, forKey: tipKey)
+        self.view.backgroundColor = customGreen
+        billTitle.textColor = UIColor .white
+        tipTitle.textColor = UIColor .white
+        totalTitle.textColor = UIColor .white
+        barDivider.backgroundColor = UIColor .white
+        tipLabel.textColor = UIColor .white
+        totalLabel.textColor = UIColor .white
+        billField.textColor = UIColor .black
+        splitPersonLabel.textColor = UIColor .white
+        splitTotalLabel.textColor = UIColor .white
+        splitField.textColor = UIColor .white
+        splitTotalValue.textColor = UIColor .white
+        tipControl.tintColor = UIColor .white
+        
+        // Set the initial selected segment to be the first one
+        tipControl.selectedSegmentIndex = 0
+        
+        // Set the initial tipPercent
+        defaults.set(tipPercentages[0], forKey: tipKey)
+        
+        // Set the rounded tip/total and split check option to false
         defaults.set(false, forKey: roundedKey)
+        defaults.set(false, forKey: splitKey)
         
         defaults.synchronize()
         
@@ -82,39 +106,42 @@ class ViewController: UIViewController {
         let roundedTotal = round(total)
         let roundedTip = roundedTotal - bill
         var numOfSplits = Double(splitField.text!) ?? 1
+        
         if(numOfSplits < 1){
             numOfSplits = 1
         }
         
-        let splitTotal = total/numOfSplits
-        
-        //set the labels
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-        splitTotalValue.text = String(format: "$%.2f", splitTotal)
+        let splitTotal = (ceil((total * 100)/numOfSplits))/100
+        let roundedSplitTotal = (ceil((roundedTotal * 100)/numOfSplits))/100
         
         let rounded = defaults.bool(forKey: roundedKey)
         
         if(rounded){
             tipTitle.text = "Tip (Rounded)"
             totalTitle.text = "Total (Rounded)"
-            tipLabel.text = tipLabel.text! + " (" + String(format: "$%.2f", roundedTip) + ")"
-            totalLabel.text = totalLabel.text! + " (" + String(format: "$%.2f", roundedTotal) + ")"
+            splitTotalLabel.text = "Split Total (Rounded)"
+            tipLabel.text = String(format: "$%.2f", roundedTip)
+            totalLabel.text = String(format: "$%.2f", roundedTotal)
+            splitTotalValue.text = String(format: "$%.2f", roundedSplitTotal)
         }
         else{
             tipTitle.text = "Tip"
             totalTitle.text = "Total"
-            tipLabel.text = tipLabel.text!
-            totalLabel.text = totalLabel.text!
+            splitTotalLabel.text = "Split Total"
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+            splitTotalValue.text = String(format: "$%.2f", splitTotal)
         }
         
     }
 
     @IBAction func tipValueChange(_ sender: Any) {
+        
+        // Set the tipPercent to the selected tip percentage and recalculate
         tipPercent = tipPercentages[tipControl.selectedSegmentIndex]
         calculateTip(self)
         
-        //save the key to UserDefaults
+        // Save the key to UserDefaults
         defaults.set(tipPercent, forKey: tipKey)
         defaults.synchronize()
     }
@@ -138,6 +165,20 @@ class ViewController: UIViewController {
         tipPercentages[1] = custom2
         tipPercentages[2] = custom3
         
+        // Load the default tip key
+        tipPercent = defaults.double(forKey: tipKey)
+        
+        // Set the selected tipControl segment
+        if(tipPercent == tipPercentages[0]){
+            tipControl.selectedSegmentIndex = 0
+        }
+        else if(tipPercent == tipPercentages[1]){
+            tipControl.selectedSegmentIndex = 1
+        }
+        else{
+            tipControl.selectedSegmentIndex = 2
+        }
+
         // Multiply the custom tip percentages by 100
         custom1 = custom1 * 100
         custom2 = custom2 * 100
@@ -148,21 +189,13 @@ class ViewController: UIViewController {
         tipControl.setTitle(String(format: "%.0f", custom2) + "%", forSegmentAt: 1)
         tipControl.setTitle(String(format: "%.0f", custom3) + "%", forSegmentAt: 2)
 
-        tipPercent = defaults.double(forKey: tipKey)
-        
-        if(tipPercent == tipPercentages[0]){
-            tipControl.selectedSegmentIndex = 0
-        }
-        else if(tipPercent == tipPercentages[1]){
-            tipControl.selectedSegmentIndex = 1
-        }
-        else{
-            tipControl.selectedSegmentIndex = 2
-        }
+        // Recalculate the tip
         calculateTip(self)
         
+        // Load the split check boolean
         let split = defaults.bool(forKey: splitKey)
         
+        // Show/hide the split check labels depending on the split boolean
         if(split){
             showSplitLabels()
         }
